d0d4036192f223f825379e5f079cc7881b11d3d6
Arif Akdeniz <arifakdnz@outlook.com>
2020-08-28 10:09:27 +0300

    Flask First Commit

---
+++ todo.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, url_for,request
+from flask_sqlalchemy import SQLAlchemy
+
+#https://flask-sqlalchemy.palletsprojects.com/en/2.x/ 
+#Bu sayfadan çok yararlandık
+
+#Bu kodda SQL sorgusu yazmdan SQL_Alchemy kullanarak database'i kontrol edeceğiz
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/hp/Desktop/TodoApp/todo.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template("index.html",todos = todos)
+
+@app.route('/add', methods = ["POST"])
+def addTodo():
+    title = request.form.get("title")
+    newTodo = Todo(title = title, complete = False)
+    db.session.add(newTodo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route('/complete/<string:id>')
+def completeTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+    
+    """if todo.complete == True :
+        todo.complete = False
+    else:
+        todo.complete = True"""
+    
+    todo.complete = not todo.complete #Kodu kısaltmak için bunu kullandık
+    
+    db.session.commit()
+
+    return redirect(url_for("index"))    
+
+
+@app.route('/delete/<string:id>')
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id = id).first()
+
+    db.session.delete(todo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #primary key olsun ve auto increment şekilde artsın
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+    
+
+if __name__ == "__main__":
+    db.create_all()     #Bu sadece 1 sefer çalışır. Her kodu çalıştırdığımızda yeni tablo oluşturmaz.
+    app.run(debug = True)
+ 
+
